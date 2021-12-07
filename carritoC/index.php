143c7e656baba092f040b231a919dd3ce8fd7936
ccuervo <bccuervo0@misena.edu.co>
2021-12-07 02:37:45 -0500

    carritoC

---
+++ carritoC/index.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Carrito De Compras</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+</head>
+
+<body>
+    <header>
+        <h1>Carrito De Compras</h1>
+        <a href="./carritodecompras.php" title="ver carrito de compras">
+            <img src="./imagenes/carrito.png">
+        </a>
+    </header>
+    <section>
+        <div class="producto">
+             <center>
+
+                 <img src="./productos/cebolla.jpg" alt=""><br>
+                 <span>Cebolla Roja</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/tomate.jpg" alt=""><br>
+                 <span>Tomate</span><br>
+                 <a class="btn btn-1 outer-shadow hover-in-shadow " href="./detalles.php?id=22">ver</a>
+             </center>
+        </div>
+        
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/papa.jpg" alt=""><br>
+                 <span>Papa</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/arveja.jpg" alt=""><br>
+                 <span>Arveja</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/arracacha.jpg" alt=""><br>
+                 <span>Arracacha</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/banano.jpg" alt=""><br>
+                 <span>Banano</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/brocoli.jpg" alt=""><br>
+                 <span>Brocoli</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/cebollacab.jpg" alt=""><br>
+                 <span>Cebolla Blanca</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/mango.jpg" alt=""><br>
+                 <span>Mango</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/espinaca.jpg" alt=""><br>
+                 <span>Espinaca</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/zanahoria.jpg" alt=""><br>
+                 <span>Zanahoria</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/guanabana.jpg" alt=""><br>
+                 <span>Guanabana</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/maracuya.jpg" alt=""><br>
+                 <span>Maracuya</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/habichuela.jpg" alt=""><br>
+                 <span>Habichuela</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+             <center>
+                 <br>
+                 <img src="./productos/lechuga.jpg" alt=""><br>
+                 <span>Lechuga</span><br>
+                 <a href="./detalles.php">ver</a>
+             </center>
+        </div>
+
+        <div class="producto">
+            <center>
+                <br>
+                <img src="./productos/manzana.jpg" alt=""><br>
+                <span>Manzana Roja</span><br>
+                <a href="./detalles.php">ver</a>
+            </center>
+        </div>
+
+        <div class="producto">
+            <center>
+                <br>
+                <img src="./productos/platano.jpg" alt=""><br>
+                <span>Platano</span><br>
+                <a href="./detalles.php">ver</a>
+            </center>
+        </div>
+
+        <div class="producto">
+            <center>
+                <br>
+                <img src="./productos/pimenton.jpg" alt=""><br>
+                <span>Pimenton</span><br>
+                <a href="./detalles.php">ver</a>
+            </center>
+        </div>
+
+        <div class="producto">
+            <center>
+                <br>
+                <img src="./productos/yuca.jpg" alt=""><br>
+                <span>Yuca</span><br>
+                <a href="./detalles.php">ver</a>
+            </center>
+        </div>
+
+        <div class="producto">
+            <center>
+                <br>
+                <img src="./productos/pepino.jpg" alt=""><br>
+                <span>Pepino</span><br>
+                <a href="./detalles.php">ver</a>
+            </center>
+        </div>
+
+        
+    </section>
+
+    
+
+
+<!-- Contact Section -->
+<section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Informacion<span>Contactos</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Telefono</h1>
+            <h2>+57 3014391260</h2>
+            <h2>+1 234 123 1234</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>bccuervo0@misena.edu.co</h2>
+            <h2>darkcrow-1@hotmail.com</h2>
+          </div>
+        </div>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Ubicacion</h1>
+            <h2>Cundinamarca, Soacha</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
+
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>C</span>amilo <span>C</span>uervo</h1>
+      </div>
+      <h2>Solucion Wed</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
+        </div>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      
+  </section>
+  <!-- End Footer -->
+  <script src="../../indexJava/app.js"></script>
+</body>
+
+</html>
+</body>
+<?php
+include './conexion.php';
+?>
+</html>
